059aaf1095c622ad8b821506d65d40a81f9896f7
Stormy <stormcloud.enterprises@gmail.com>
2020-02-27 23:57:18 -0800

    redid forms w/vertical orientation; adjusted some responsive stuff w/BS; asst.styling

--- Petopia/Petopia/Petopia/Models/Owner.cs
+++ Petopia/Petopia/Petopia/Models/Owner.cs
@@ -2,6 +2,7 @@ namespace Petopia.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -16,10 +17,12 @@ namespace Petopia.Models
         public string Username { get; set; }
 
         [Required]
+        [DisplayName("First Name:")]
         [StringLength(120)]
         public string FirstName { get; set; }
 
         [Required]
+        [DisplayName("Last Name:")]
         [StringLength(120)]
         public string LastName { get; set; }
     }
