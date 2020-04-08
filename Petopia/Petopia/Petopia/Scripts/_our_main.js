73492d5f2069c38582da7457ffab3ec2ea556eb9
Joeyyau <jtavares15@mail.wou.edu>
2020-04-08 13:06:39 -0700

    Added the Admin role to startup.cs as well as added the first inital character counter in the PetOwner/Create/

--- Petopia/Petopia/Petopia/Scripts/_our_main.js
+++ Petopia/Petopia/Petopia/Scripts/_our_main.js
@@ -59,4 +59,17 @@ function scaleBannerVideoSize(element) {
 
  
 }
+
+//Implementation of text area character counter.
+var text_max = 200;
+$('#count_message').html('0 / ' + text_max);
+
+$('#text').keyup(function () {
+    var text_length = $('#text').val().length;
+    var text_remaining = text_max - text_length;
+
+    $('#count_message').html(text_length + ' / ' + text_max);
+});
+
+
 //=======================================================================================
