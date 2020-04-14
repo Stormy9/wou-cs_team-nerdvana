eba002ee5f9a9ca73b06ea99a6dc302b0c6d46e4
Stormy <stormcloud.enterprises@gmail.com>
2020-04-14 15:59:23 -0700

    finished out user profile details - profile page, create, edit all seems to work!

--- Petopia/Petopia/Petopia/Controllers/ProfilePageController.cs
+++ Petopia/Petopia/Petopia/Controllers/ProfilePageController.cs
@@ -15,10 +15,13 @@ namespace Petopia.Controllers
     public class ProfilePageController : Controller
     {
         private PetopiaContext db = new PetopiaContext();
+
+        //===============================================================================
         // GET: ProfilePage
         public ActionResult Index()
         {
             var identityID = User.Identity.GetUserId();
+
             var loggedID = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.UserID).First();
             
             ProfileViewModel petopiaUser = new ProfileViewModel();
@@ -29,41 +32,64 @@ namespace Petopia.Controllers
                                                 po => po.UserID,
                                                 (pu, po) => new {PetUse = pu, PetOwn = po }) */
             //linq isnt populating correctly right now so we're doing it manually (TEMP FIX)
+            
             petopiaUser.UserID = loggedID;
             petopiaUser.FirstName = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.FirstName).First();
             petopiaUser.LastName = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.LastName).First();
+            
             petopiaUser.IsOwner = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.IsOwner).First();
             petopiaUser.IsProvider = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.IsProvider).First();
-            petopiaUser.ResState = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResState).First();
+            
+            
 
-            //This stuff probably won't be displayed in the profile but will be put into the model just in case
+            // This stuff probably won't be displayed in the profile 
+            //    but will be put into the model just in case
             petopiaUser.MainPhone = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.MainPhone).First();
             petopiaUser.AltPhone = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.AltPhone).First();
             petopiaUser.ResAddress01 = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResAddress01).First();
             petopiaUser.ResAddress02 = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResAddress02).First();
             petopiaUser.ResCity = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResCity).First();
+            petopiaUser.ResState = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResState).First();
             petopiaUser.ResZipcode = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResZipcode).First();
 
 
             petopiaUser.ProfilePhoto = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ProfilePhoto).First();
 
+
+            petopiaUser.UserCaption = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.UserCaption).First();
+
+            petopiaUser.Tagline = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.Tagline).First();
+
+            petopiaUser.GeneralLocation = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.GeneralLocation).First();
+
+            petopiaUser.UserBio = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.UserBio).First();
+
+
+            //---------------------------------------------------------------------------
             //We might not have these so we want to see if we get a result back before populating...
             if (db.CareProviders.Where(x => x.UserID == loggedID).Count() == 1)
-            { //If we get 1, then we know this user is a CareProvider
+            { 
+                //If we get 1, then we know this user is a CareProvider
                 petopiaUser.ProviderAverageRating = db.CareProviders.Where(x => x.UserID == loggedID).Select(x => x.AverageRating).First();
+
                 petopiaUser.ExperienceDetails = db.CareProviders.Where(x => x.UserID == loggedID).Select(x => x.ExperienceDetails).First();
             }
+
             if(db.PetOwners.Where(x => x.UserID == loggedID).Count() == 1)
             {
                 petopiaUser.OwnerAverageRating = db.PetOwners.Where(x => x.UserID == loggedID).Select(x => x.AverageRating).First();
+
                 petopiaUser.NeedsDetails = db.PetOwners.Where(x => x.UserID == loggedID).Select(x => x.NeedsDetails).First();
+
                 petopiaUser.AccessInstructions = db.PetOwners.Where(x => x.UserID == loggedID).Select(x => x.AccessInstructions).First();
             }
 
+            //---------------------------------------------------------------------------
             //Tests to make getting pets easier
             if (db.PetOwners.Where(x => x.UserID == loggedID).Count() == 1)
             {
                 int ownerID = db.PetOwners.Where(x => x.UserID == loggedID).Select(x => x.PetOwnerID).First();
+
                 petopiaUser.PetList = db.Pets.Where(x => x.PetOwnerID == ownerID).Select(n => new PetInfo
                     {
                         PetName = n.PetName,
@@ -75,6 +101,7 @@ namespace Petopia.Controllers
                     }).ToList();
             }
 
+            //---------------------------------------------------------------------------
             //Check if badges exist and put in ViewBag.
             DAL.UserBadge UserBadges = db.UserBadges.Where(x => x.UserID == loggedID).FirstOrDefault();
             if (UserBadges == null)
@@ -95,42 +122,64 @@ namespace Petopia.Controllers
 
             return View(petopiaUser);
         }
-
+        //===============================================================================
+        //===============================================================================
         // GET: ProfilePage/Edit
         public ActionResult Edit()
         {
             var identityID = User.Identity.GetUserId();
+
             var loggedID = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.UserID).First();
 
             ProfileViewModel petopiaUser = new ProfileViewModel();
 
             petopiaUser.FirstName = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.FirstName).First();
             petopiaUser.LastName = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.LastName).First();
+
+
             petopiaUser.IsOwner = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.IsOwner).First();
             petopiaUser.IsProvider = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.IsProvider).First();
-            petopiaUser.ResState = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResState).First();
+
+            
 
             petopiaUser.MainPhone = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.MainPhone).First();
             petopiaUser.AltPhone = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.AltPhone).First();
             petopiaUser.ResAddress01 = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResAddress01).First();
             petopiaUser.ResAddress02 = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResAddress02).First();
             petopiaUser.ResCity = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResCity).First();
+            petopiaUser.ResState = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResState).First();
             petopiaUser.ResZipcode = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ResZipcode).First();
 
-            //We might not have these so we want to see if we get a result back before populating...
+
+            petopiaUser.UserCaption = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.UserCaption).First();
+
+            petopiaUser.Tagline = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.Tagline).First();
+
+            petopiaUser.GeneralLocation = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.GeneralLocation).First();
+
+            petopiaUser.UserBio = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.UserBio).First();
+
+
+            //---------------------------------------------------------------------------
+            //We might not have these so we want to see if we get a result back 
+            //     before populating...
             if (db.CareProviders.Where(x => x.UserID == loggedID).Count() == 1)
-            { //If we get 1, then we know this user is a CareProvider
+            { 
+                //If we get 1, then we know this user is a CareProvider
                 petopiaUser.ExperienceDetails = db.CareProviders.Where(x => x.UserID == loggedID).Select(x => x.ExperienceDetails).First();
             }
+
             if (db.PetOwners.Where(x => x.UserID == loggedID).Count() == 1)
             {
                 petopiaUser.NeedsDetails = db.PetOwners.Where(x => x.UserID == loggedID).Select(x => x.NeedsDetails).First();
+
                 petopiaUser.AccessInstructions = db.PetOwners.Where(x => x.UserID == loggedID).Select(x => x.AccessInstructions).First();
             }
+            //---------------------------------------------------------------------------
 
             return View(petopiaUser);
         }
-
+        //-------------------------------------------------------------------------------
         // POST: ProfilePage/Edit
         [HttpPost]
         [AllowAnonymous]
@@ -140,16 +189,23 @@ namespace Petopia.Controllers
             if (ModelState.IsValid)
             {
                 var identityID = User.Identity.GetUserId();
+
                 var loggedID = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.UserID).First();
+
                 //Need to split up model into 3 objects to fit into db
+
                 //PetopiaUser
                 DAL.PetopiaUser currentUser = new DAL.PetopiaUser();
+
                 currentUser.UserID = loggedID;
+
                 currentUser.FirstName = model.FirstName;
                 currentUser.LastName = model.LastName;
+
                 currentUser.ASPNetIdentityID = identityID;
                 currentUser.IsOwner = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.IsOwner).First();
                 currentUser.IsProvider = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.IsProvider).First();
+
                 currentUser.MainPhone = model.MainPhone;
                 currentUser.AltPhone = model.AltPhone;
                 currentUser.ResAddress01 = model.ResAddress01;
@@ -158,9 +214,18 @@ namespace Petopia.Controllers
                 currentUser.ResState = model.ResState;
                 currentUser.ResZipcode = model.ResZipcode;
 
+                currentUser.UserCaption = model.UserCaption;
+                currentUser.GeneralLocation = model.GeneralLocation;
+                currentUser.UserBio = model.UserBio;
+                currentUser.Tagline = model.Tagline;
+
+                //-----------------------------------------------------------------------
                 //Need to put the bools back into the model just in case we have to return
                 model.IsOwner = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.IsOwner).First();
+
                 model.IsProvider = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.IsProvider).First();
+               
+                //-----------------------------------------------------------------------
                 //save PetopiaUser into db
                 if (model.UserProfilePicture != null)
                 {
@@ -169,16 +234,22 @@ namespace Petopia.Controllers
                         ModelState.AddModelError("CustomError", "Image can not be lager than 4MB.");
                         return View(model);
                     }
+
                     if (!(model.UserProfilePicture.ContentType == "image/jpeg"))
                     {
                         ModelState.AddModelError("CustomError", "Image must be in jpeg format.");
                         return View(model);
                     }
+
                     byte[] data = new byte[model.UserProfilePicture.ContentLength];
+                    
                     model.UserProfilePicture.InputStream.Read(data, 0, model.UserProfilePicture.ContentLength);
+
                     currentUser.ProfilePhoto = data;
                 }
-                else //If no pic was uploaded, we need to seed the current profile pic into our user
+
+                //If no pic was uploaded, we need to seed the current profile pic into our user
+                else
                 {
                     currentUser.ProfilePhoto = db.PetopiaUsers.Where(x => x.ASPNetIdentityID == identityID).Select(x => x.ProfilePhoto).First();
                 }
@@ -187,36 +258,55 @@ namespace Petopia.Controllers
                 db.Entry(currentUser).State = EntityState.Modified;
                 db.SaveChanges();
 
+                //-----------------------------------------------------------------------
                 //Care Provider
                 if (db.CareProviders.Where(x => x.UserID == loggedID).Count() == 1)
-                { //If we get 1, then we know this user is a CareProvider
+                { 
+                    //If we get 1, then we know this user is a CareProvider
                     DAL.CareProvider currentProvider = new DAL.CareProvider();
+                    
                     currentProvider.CareProviderID = db.CareProviders.Where(x => x.UserID == loggedID).Select(x => x.CareProviderID).First();
+
                     currentProvider.AverageRating = db.CareProviders.Where(x => x.UserID == loggedID).Select(x => x.AverageRating).First();
+
                     currentProvider.ExperienceDetails = model.ExperienceDetails;
+
                     currentProvider.UserID = loggedID;
+                    
                     //Save CareProvider into db
                     db.Entry(currentProvider).State = EntityState.Modified;
                     db.SaveChanges();
                 }
+                //-----------------------------------------------------------------------
                 //Pet Owner
                 if (db.PetOwners.Where(x => x.UserID == loggedID).Count() == 1)
-                { //If we get 1, then we know this user is a PetOwner
+                { 
+                    //If we get 1, then we know this user is a PetOwner
                     DAL.PetOwner currentOwner = new DAL.PetOwner();
+                    
                     currentOwner.PetOwnerID = db.PetOwners.Where(x => x.UserID == loggedID).Select(x => x.PetOwnerID).First();
+
                     currentOwner.AverageRating = db.PetOwners.Where(x => x.UserID == loggedID).Select(x => x.AverageRating).First();
+
                     currentOwner.NeedsDetails = model.NeedsDetails;
+
                     currentOwner.AccessInstructions = model.AccessInstructions;
+
                     currentOwner.UserID = loggedID;
+                    
                     //Save PetOwner into db
                     db.Entry(currentOwner).State = EntityState.Modified;
                     db.SaveChanges();
                 }
+                //-----------------------------------------------------------------------
                 //db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
+            //===========================================================================
             //If we got here something bad happened, return model
             return View(model);
+
         }
     }
 }
