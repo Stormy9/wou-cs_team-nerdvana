b9c1e8ffee0e085762ae9fb3bc257335e2898d92
Stormy <stormcloud.enterprises@gmail.com>
2020-04-15 23:01:19 -0700

    CareTransactionsController + profile page (what on that??)

--- Petopia/Petopia/Petopia/Controllers/CareTransactionsController.cs
+++ Petopia/Petopia/Petopia/Controllers/CareTransactionsController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using Petopia.Models;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using Petopia.Models;
 
 namespace Petopia.Controllers
 {
