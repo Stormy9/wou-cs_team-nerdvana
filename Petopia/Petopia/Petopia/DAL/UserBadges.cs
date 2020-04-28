35c65a112326499b9194c5a624200c708514d6d2
Corrin Moser <cmoser18@mail.wou.edu>
2020-04-28 14:56:55 -0700

    Added images for badges, and changed model to fit the new badges.

--- Petopia/Petopia/Petopia/DAL/UserBadges.cs
+++ Petopia/Petopia/Petopia/DAL/UserBadges.cs
@@ -23,10 +23,39 @@ namespace Petopia.DAL
         public bool CatOwner { get; set; }
         [DisplayName("Cat ")]
         public bool CatProvider { get; set; }
-        [DisplayName("Bunny ")]
+        [DisplayName("Bird ")]
         public bool BirdOwner { get; set; }
-        [DisplayName("Bunny ")]
+        [DisplayName("Bird ")]
         public bool BirdProvider { get; set; }
+        [DisplayName("Fish ")]
+        public bool FishOwner { get; set; }
+        [DisplayName("Fish ")]
+        public bool FishProvider { get; set; }
+        [DisplayName("Horse ")]
+        public bool HorseOwner { get; set; }
+        [DisplayName("Horse ")]
+        public bool HorseProvider { get; set; }
+        [DisplayName("Livestock ")]
+        public bool LivestockOwner { get; set; }
+        [DisplayName("Livestock ")]
+        public bool LivestockProvider { get; set; }
+        [DisplayName("Rabbit ")]
+        public bool RabbitOwner { get; set; }
+        [DisplayName("Rabbit ")]
+        public bool RabbitProvider { get; set; }
+        [DisplayName("Reptile ")]
+        public bool ReptileOwner { get; set; }
+        [DisplayName("Reptile ")]
+        public bool ReptileProvider { get; set; }
+        [DisplayName("Small rodents ")]
+        public bool RodentOwner { get; set; }
+        [DisplayName("Small rodents ")]
+        public bool RodentProvider { get; set; }
+        [DisplayName("Other ")]
+        public bool OtherOwner { get; set; }
+        [DisplayName("Other ")]
+        public bool OtherProvider { get; set; }
+
 
         public virtual PetopiaUser PetopiaUser { get; set; }
     }
